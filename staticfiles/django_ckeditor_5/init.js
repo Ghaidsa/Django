45275ec67d15ea6c5ddd2bc41b1b6d268411caaf
Ghaidsa <ghaidsaaadjiraa@gmail.com>
2025-06-26 18:51:54 +0800

    Remove large folders from repo to fix Vercel size limit

--- staticfiles/django_ckeditor_5/init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('.django_ckeditor_5').forEach((el) => {
-    ClassicEditor
-      .create(el)
-      .catch(error => {
-        console.error('CKEditor error:', error);
-      });
-  });
-});
